e57bb212b7f6073d371f1a5023bff8b95be98043
Anh <anhnguyenis@gmail.com>
2019-03-27 19:31:21 +0000

    HTML page added

--- Spotipy_quickstart.py
+++ Spotipy_quickstart.py
@@ -1,12 +1,30 @@
 import spotipy
-import spotipy.util as util
-from config import CLIENT_ID, CLIENT_SECRET, PLAY_LIST, USER
-import random
+from spotipy import oauth2
+from pprint import pprint
 
-token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+client_ID = 'e9ddaa3b0c35491b8ab175459ccdf2fe'
+client_secret = '074f6e3ddfe34b74a35985161ba264c1'
 
-cache_token = token.get_access_token()
-spotify = spotipy.Spotify(cache_token)
+auth = oauth2.SpotifyClientCredentials\
+    (client_id = client_ID, client_secret =client_secret)
 
-results1 = spotify.user_playlist_tracks(USER, PLAY_LIST, limit=100, offset=0)
+token = auth.get_access_token()
+spotify = spotipy.Spotify(auth=token)
+
+results = spotify.search(q='artist:shakira', type='artist')
+#pprint(dir(spotify))
+pprint(results)
+
+
+# import random
+#
+# token = util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+#
+# cache_token = token.get_access_token()
+# spotify = spotipy.Spotify(cache_token)
+#
+# results1 = spotify.user_playlist_tracks(USER, PLAY_LIST, limit=100, offset=0)
+#
+# 81
+# ddca1ee4774aa5bd5343beec81c8f1
 
--- food2fork.py
+++ food2fork.py
@@ -3,6 +3,8 @@ from pprint import pprint
 
 api_key = '8d4b3dab2ea6f06e692d7cd775f149b7'
 ingredient = 'avocado'
+
 response = requests.get('https://www.food2fork.com/api/search?key={}&q={}'.format(api_key, ingredient))
 
 pprint(response.json())
+
