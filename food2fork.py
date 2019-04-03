e6aa4fc6bddb37eff5870a6298ee214ce5f2d6c4
Anh <anhnguyenis@gmail.com>
2019-04-03 14:38:02 +0100

    routes added to html Homepage, About page and Signup page
    API recipe call function added

--- food2fork.py
+++ food2fork.py
@@ -1,10 +1,19 @@
 import requests
 from pprint import pprint
 
-api_key = '8d4b3dab2ea6f06e692d7cd775f149b7'
-ingredient = 'avocado'
+def get_recipes():
+    api_key = '6057e26d2643268985352767e47fd9d1'
+    ingredient = 'chicken,tomato,'
+    #ingredient = input('What ingredients do you have? ')
+    response = requests.get('https://www.food2fork.com/api/search?key={}&q={}'.format(api_key, ingredient))
 
-response = requests.get('https://www.food2fork.com/api/search?key={}&q={}'.format(api_key, ingredient))
+    data = response.json()
+    #pprint(response.json())
 
-pprint(response.json())
+    recipe = data
+
+    #print(recipe['count'])
+    #print(recipe['recipes'])
+    #pprint(recipe['recipes'])
+    return (recipe)
 
