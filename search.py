6c3148b89ae507ade4c85458b2a48e58e6fa17ee
Anh <anhnguyenis@gmail.com>
2019-04-10 15:41:59 +0100

    Overlay navigation bar added. Font colors adjusted. About page updated with block quote and containers.

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
-    search = StringField('Type in all your ingredients here (separate each ingredient with a comma ,)', validators=[DataRequired()])
+    search = StringField('(separate each ingredient with a comma ,)', validators=[DataRequired()])
     #password = PasswordField('Password', validators=[DataRequired()])
     #remember_me = BooleanField('Remember Me')
     submit = SubmitField('Search')
