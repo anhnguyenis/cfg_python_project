b9eaf27a75c5452b6a0e9746c16c30359c3eb914
Anh <anhnguyenis@gmail.com>
2019-04-17 19:34:20 +0100

    stuff added

--- food2fork.py
+++ food2fork.py
@@ -3,8 +3,10 @@ from pprint import pprint
 
 
 def get_recipe(ingredient):
-    api_key = '33aaefd0024a6798b6324f2670c67a4f'
 
+    api_key = '6057e26d2643268985352767e47fd9d1'
+    #ingredient = 'chicken,tomato,'
+    ingredient = input('What ingredients do you have? ')
     response = requests.get('https://www.food2fork.com/api/search?key={}&q={}'.format(api_key, ingredient))
 
     data = response.json()
